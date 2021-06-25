a2b618ee8cec8b6ae34bd4cba38e28d19f777211
Huy Nguyen <huynn109@gmail.com>
2021-06-25 14:18:16 +0700

    styles

--- app/src/main/java/com/huynn109/template/data/model/res/BaseRes.kt
+++ app/src/main/java/com/huynn109/template/data/model/res/BaseRes.kt
@@ -5,7 +5,9 @@ import com.google.gson.annotations.SerializedName
 /**
  * Created by huynn109 on 2019-07-01.
  */
-abstract class BaseRes(@SerializedName("status")
-                            var code: Int? = -1,
-                            @SerializedName("message")
-                            var message: String? = null)
+abstract class BaseRes(
+    @SerializedName("status")
+    var code: Int? = -1,
+    @SerializedName("message")
+    var message: String? = null
+)
